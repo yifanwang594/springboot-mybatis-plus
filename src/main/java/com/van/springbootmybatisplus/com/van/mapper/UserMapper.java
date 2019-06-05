459ddc30ef582bf81887a9f4c3f4d140e374f881
Van <yifan_wang594@163.com>
2019-06-05 22:39:10 +0800

    测试Git

---
+++ src/main/java/com/van/springbootmybatisplus/com/van/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.van.springbootmybatisplus.com.van.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.van.springbootmybatisplus.com.van.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+
+
+}
